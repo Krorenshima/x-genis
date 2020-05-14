7910455bfcbde5cd3905984a776a38f51f496fbf
Jupyter <official.chubbyroo@gmail.com>
2020-05-14 08:10:36 -0700

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -1,12 +1,22 @@
 let mouse, mmove;
 
 mouse = {
-  x: 0, y: 0,
   l: {x: 0, y: 0},
-  get delt () {return {x: this.l.x - this.x, y: this.l.y - this.y}},
-  get abs () {return {x: Math.abs(this.delt.x), y: Math.abs(this.delt.y)}},
+  ll: {x: 0, y: 0},
+  get delt () {
+    if (!this.l.x > 0) {return null}
+    if (!this.ll.x > 0) {return null}
+    return {x: this.l.x - this.x, y: this.l.y - this.y}
+  },
+  get abs () {
+    if (this.delta == null) {return null}
+    return {x: Math.abs(this.delt.x), y: Math.abs(this.delt.y)}
+  },
+  _direc () {
+    this.direction = mouse.abs.x > mouse.abs.y ? (mouse.delta.x > 0 ? 'left' : 'right') : (mouse.delta.y > 0 ? 'up', 'down');
+  },
+  direction: null,
   pressed: !1,
-  direc: '',
   chdirec: !1, // changed direction
   moving: !1,
   cursor (icon) {cvs.style.cursor = icon}
@@ -15,12 +25,11 @@ mmove = setTimeout(() => {
   
 }, 1000)
 window.addEventListener('mousemove', (e) => {
-  mouse.x = e.clientX - cvs.offsetLeft;
-  mouse.y = e.clientY - cvs.offsetTop;
+  mouse.l.x = e.clientX - cvs.offsetLeft; mouse.l.y = e.clientY - cvs.offsetTop;
   
-  mouse.direc = abs.x > abs.y ? (delta.x > 0 ? 'Left' : 'Right') : (delta.y > 0 ? 'Up' : 'Down');
+  mouse._direc();
   
-  mouse.l = {x: mouse.x, y: mouse.y}
+  mouse.ll.x = mouse.l.x; mouse.ll.y = mouse.l.y;
 });
 window.addEventListener('mouseup', () => mouse.pressed = !1);
 window.addEventListener('mousedown', () => mouse.pressed = !0);
